22c3beab5f8a8dd50c17bf9085005adc52a6b3c7
Hippie Hacker <hh@M1Max.local>
2022-03-29 15:20:11 +1300

    add org/.dir-locals.el

---
+++ org/.dir-locals.el
@@ -0,0 +1,58 @@
+((
+  org-mode
+  .
+  (
+   (
+    eval
+    .
+    (progn
+      (message "START: ii/sql-org-hacks")
+      (defun reconfigure-org ()
+        (interactive)
+      (set (make-local-variable 'org-babel-default-header-args:sql-mode)
+            ;; Set up all sql-mode blocks to be postgres and literate
+            '((:results . "replace code")
+              (:product . "postgres")
+              (:session . "infrasnoop")
+              (:noweb . "yes")
+              (:comments . "no")
+              (:wrap . "SRC example")))
+      (set (make-local-variable 'sql-server)
+           (if (getenv "PGHOST")
+               (getenv "PGHOST")
+             (if (file-exists-p "/var/run/secrets/kubernetes.io/serviceaccount/namespace")
+                 "k8s-infra.infrasnoop"
+               "localhost"
+               )))
+      (set (make-local-variable 'sql-port)
+           (if (getenv "PGPORT")
+               (string-to-number (getenv "PGPORT"))
+             5432))
+      (set (make-local-variable 'sql-user)
+           (if (getenv "PGUSER")
+               (getenv "PGUSER")
+             "k8s-infra"))
+      (set (make-local-variable 'sql-database)
+           (if (getenv "PGDATABASE")
+               (getenv "PGDATABASE")
+             "k8s-infra"))
+      (set (make-local-variable 'sql-product)
+           '(quote postgres))
+      (set (make-local-variable 'sql-connection-alist)
+           (list
+            ;; setting these allows for the connection to be
+            ;; created on the fly
+            (list 'infrasnoop
+                  (list 'sql-product '(quote postgres))
+                  (list 'sql-database (concat "postgresql://"
+                                          sql-user
+                                          (if (getenv "PGPASSWORD")
+                                              (concat ":" (getenv "PGPASSWORD")))
+                                          "@" sql-server
+                                          ":" (number-to-string sql-port)
+                                          "/" sql-database ;; replace with your database
+                                          )))))
+      )
+      (reconfigure-org)
+      (message "END: ii/sql-org-hacks")
+      )))))
