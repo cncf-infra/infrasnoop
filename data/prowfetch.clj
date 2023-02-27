c03d1ad536e45e9c31bd92a027ab4f0345d12a5c
zim <webmaster@coolguy.website>
2023-02-28 11:33:53 +1300

    wip job text experiment

--- data/prowfetch.clj
+++ data/prowfetch.clj
@@ -1,6 +1,7 @@
 (ns prowfetch
   (:require [clojure.string :as str]
             [clojure.java.io :as io]
+            [org.httpkit.client :as http]
             [babashka.fs :as fs]
             [cheshire.core :as json]
             [clj-yaml.core :as yaml]
@@ -34,6 +35,32 @@
        (format "[%s]")
        (spit outfile)))
 
+
 (files->json "./test-infra/config/jobs/" "**.yaml" "prow-jobs.json")
 (files->json "./test-infra/" "**OWNERS" "owners.json")
 (spit "prow-deck.json" (:body (curl/get "https://prow.k8s.io/data.js")))
+
+(defn url->log-url [url]
+  (let [new-url (str/replace url #"prow.k8s.io/view/gs" "storage.googleapis.com")]
+    (str new-url
+         (when (not (str/ends-with? new-url "/")) "/")
+         "build-log.txt")))
+
+
+(def sampleten
+  (take 100
+        (map #(assoc % :url (url->log-url (:url %)))
+             (map #(select-keys % [:job :build_id :url]) (filter #(= "success" (:state %)) (json/parse-string (slurp "prow-deck.json") true))))))
+
+(defn add-text
+  [job]
+  (try
+    (let [text (:body (curl/get (:url job)))]
+      (assoc job :text text))
+    (catch Exception e
+      (assoc job :text (str "ERROR: " e)))))
+
+
+(spit "test-log.json"
+      (json/generate-string
+       (pmap add-text sampleten)))
