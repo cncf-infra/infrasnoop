c80fd5bbb12b482c2bda53b2919dd86452602b87
zim <webmaster@coolguy.website>
2023-03-01 12:44:46 +1300

    Add webscraping to prowfetch

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,20 @@
-FROM postgres:15
+FROM --platform=linux/amd64 postgres:15-bullseye
+
+ARG BOOTLEG_VER=0.1.9
 
 # Basic dependencies
-RUN apt update && apt upgrade && apt install -y curl jq tree wget
+RUN apt update && apt upgrade -y && apt install -y curl jq tree wget
 RUN apt install -y git
 
 # Install babashka
 RUN curl -sLO https://raw.githubusercontent.com/babashka/babashka/master/install && chmod +x install && ./install
+
+# Install bootleg for web scraping
+RUN wget https://github.com/retrogradeorbit/bootleg/releases/download/v$BOOTLEG_VER/bootleg-$BOOTLEG_VER-linux-amd64.tgz
+RUN tar xvf bootleg-$BOOTLEG_VER-linux-amd64.tgz && \
+    mv bootleg /usr/local/bin && \
+    chown 0:0 root /usr/local/bin/bootleg && \
+    chmod +x /usr/local/bin/bootleg
 
 # Install yq
 RUN wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq &&\
--- data/prowfetch.clj
+++ data/prowfetch.clj
@@ -1,11 +1,14 @@
 (ns prowfetch
   (:require [clojure.string :as str]
-            [clojure.java.io :as io]
-            [org.httpkit.client :as http]
+            [babashka.pods :as pods]
             [babashka.fs :as fs]
             [cheshire.core :as json]
             [clj-yaml.core :as yaml]
             [babashka.curl :as curl]))
+
+(pods/load-pod "bootleg")
+(require '[pod.retrogradeorbit.bootleg.utils :refer [convert-to]]
+         '[pod.retrogradeorbit.hickory.select :as s])
 
 (def ref-branch
   (-> "./test-infra/.git/packed-refs"
@@ -35,32 +38,55 @@
        (format "[%s]")
        (spit outfile)))
 
+(defn url->artifact-url [url]
+  (let [new-url (str/replace url #"prow.k8s.io/view/gs" "gcsweb.k8s.io/gcs")]
+    (if (str/ends-with?  new-url "/")
+      new-url
+      (str new-url "/"))))
+
+(defn get-artifact-text! [url]
+  (try
+    (:body (curl/get url))
+    (catch Exception e
+      (println "error getting artifact text for "url ": " (ex-message e))
+      "")))
+
+(defn artifact-text->rows
+  [text]
+  (let [artifact-soup (-> text
+                          str/trim-newline
+                          (str/replace #"^\n" "")
+                          (convert-to :hickory))]
+    (->> artifact-soup
+         (s/select (s/descendant (s/class "grid-row")))
+         (drop 1))))
+
+(defn row->artifact
+  [row]
+  (let [[href size modified] (->> row :content (filter #(not (string? %))))]
+    {:href (-> href :content first :attrs :href)
+     :size (-> size :content first)
+     :modified (-> modified :content first)}))
+
+(defn add-artifacts
+  "given a job map, add an artifact key with all artifact urls in that job's gcs bucket"
+  [job]
+  (let [artifact-url (url->artifact-url (:url job))
+        text (get-artifact-text! artifact-url)
+        rows (artifact-text->rows text)
+        artifacts (map row->artifact rows)]
+    (assoc job :artifacts artifacts)))
+
+(defn successful-jobs
+  "takes a prow-deck json and gets all successful jobs, returning just the job, build, and relevant urls"
+  [deck]
+  (let [deck-json (json/parse-string (slurp deck) true)]
+    (->> deck-json
+         (filter #(= "success" (:state %)))
+         (map #(select-keys % [:job :build_id :url]))
+         (pmap add-artifacts))))
 
 (files->json "./test-infra/config/jobs/" "**.yaml" "prow-jobs.json")
 (files->json "./test-infra/" "**OWNERS" "owners.json")
 (spit "prow-deck.json" (:body (curl/get "https://prow.k8s.io/data.js")))
-
-(defn url->log-url [url]
-  (let [new-url (str/replace url #"prow.k8s.io/view/gs" "storage.googleapis.com")]
-    (str new-url
-         (when (not (str/ends-with? new-url "/")) "/")
-         "build-log.txt")))
-
-
-(def sampleten
-  (take 100
-        (map #(assoc % :url (url->log-url (:url %)))
-             (map #(select-keys % [:job :build_id :url]) (filter #(= "success" (:state %)) (json/parse-string (slurp "prow-deck.json") true))))))
-
-(defn add-text
-  [job]
-  (try
-    (let [text (:body (curl/get (:url job)))]
-      (assoc job :text text))
-    (catch Exception e
-      (assoc job :text (str "ERROR: " e)))))
-
-
-(spit "test-log.json"
-      (json/generate-string
-       (pmap add-text sampleten)))
+(spit "job-logs.json" (json/generate-string (successful-jobs "prow-deck.json")))
