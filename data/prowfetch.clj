05bf4f4815db523d6977ea80916917b472b4c2b9
zim <webmaster@coolguy.website>
2023-03-02 11:51:21 +1300

    wip try to recursively go through all files

--- data/prowfetch.clj
+++ data/prowfetch.clj
@@ -6,9 +6,34 @@
             [clj-yaml.core :as yaml]
             [babashka.curl :as curl]))
 
+
 (pods/load-pod "bootleg")
 (require '[pod.retrogradeorbit.bootleg.utils :refer [convert-to]]
          '[pod.retrogradeorbit.hickory.select :as s])
+
+(defn distinct-by
+  "Returns a stateful transducer that removes elements by calling f on each step as a uniqueness key.
+   Returns a lazy sequence when provided with a collection."
+  ([f]
+   (fn [rf]
+     (let [seen (volatile! #{})]
+       (fn
+         ([] (rf))
+         ([result] (rf result))
+         ([result input]
+          (let [v (f input)]
+            (if (contains? @seen v)
+              result
+              (do (vswap! seen conj v)
+                  (rf result input)))))))))
+  ([f xs]
+   (sequence (distinct-by f) xs)))
+
+
+(def ztest [{:name "z" :time 3} {:name "z" :time 1} {:name "h" :time 1} {:name "z" :time 2} {:name "h" :time 0}])
+
+(distinct-by :name ztest)
+
 
 (def ref-branch
   (-> "./test-infra/.git/packed-refs"
@@ -39,10 +64,17 @@
        (spit outfile)))
 
 (defn url->artifact-url [url]
-  (let [new-url (str/replace url #"prow.k8s.io/view/gs" "gcsweb.k8s.io/gcs")]
-    (if (str/ends-with?  new-url "/")
-      new-url
-      (str new-url "/"))))
+  (let [url-root "https://gcsweb.k8s.io"
+        replace-prow (fn [url] (str/replace url #"^https://prow.k8s.io/view/gs" (str url-root "/gcs")))
+        absolute-url (fn [url] (if (not (str/starts-with? url url-root))
+                                 (str url-root url) url))
+        trailing-slash (fn [url] (if (not (str/ends-with? url "/"))
+                                   (str url "/") url))]
+    (-> url
+        replace-prow
+        absolute-url
+        trailing-slash
+        )))
 
 (defn get-artifact-text! [url]
   (try
@@ -54,8 +86,8 @@
 (defn artifact-text->rows
   [text]
   (let [artifact-soup (-> text
-                          str/trim-newline
-                          (str/replace #"^\n" "")
+                          (str/replace #"(\n|\t|<!doctype html>)" "")
+                          str/trim
                           (convert-to :hickory))]
     (->> artifact-soup
          (s/select (s/descendant (s/class "grid-row")))
@@ -63,18 +95,28 @@
 
 (defn row->artifact
   [row]
-  (let [[href size modified] (->> row :content (filter #(not (string? %))))]
-    {:href (-> href :content first :attrs :href)
-     :size (-> size :content first)
-     :modified (-> modified :content first)}))
+  (let [[hrefhick sizehick modhick] (->> row :content (filter #(not (string? %))))
+        url (-> hrefhick :content first :attrs :href)
+        size (-> sizehick :content first)
+        modified (-> modhick :content first)]
+    {:url url
+     :size size
+     :modified modified}))
+
+(defn get-all-artifacts
+  [url]
+  (let [artifact-url (url->artifact-url url)
+        text (get-artifact-text! artifact-url)
+        rows (artifact-text->rows text)
+        artifacts-at-level (map row->artifact rows)]
+    (pmap #(if (str/ends-with? (:url %) "/")
+            (get-all-artifacts (:url %))
+            %) artifacts-at-level)))
 
 (defn add-artifacts
   "given a job map, add an artifact key with all artifact urls in that job's gcs bucket"
   [job]
-  (let [artifact-url (url->artifact-url (:url job))
-        text (get-artifact-text! artifact-url)
-        rows (artifact-text->rows text)
-        artifacts (map row->artifact rows)]
+  (let [artifacts (flatten (get-all-artifacts (:url job)))]
     (assoc job :artifacts artifacts)))
 
 (defn successful-jobs
@@ -83,10 +125,23 @@
   (let [deck-json (json/parse-string (slurp deck) true)]
     (->> deck-json
          (filter #(= "success" (:state %)))
+         (sort-by :started)
+         (distinct-by :job)
          (map #(select-keys % [:job :build_id :url]))
          (pmap add-artifacts))))
+
 
 (files->json "./test-infra/config/jobs/" "**.yaml" "prow-jobs.json")
 (files->json "./test-infra/" "**OWNERS" "owners.json")
 (spit "prow-deck.json" (:body (curl/get "https://prow.k8s.io/data.js")))
-(spit "job-logs.json" (json/generate-string (successful-jobs "prow-deck.json")))
+;; (spit "job-artifacts.json" (json/generate-string (successful-jobs "prow-deck.json")))
+
+
+;; (spit "job-artifacts-test.json"
+;;       (json/generate-string
+;;        (flatten
+;;         (get-all-artifacts "https://gcsweb.k8s.io/gcs/kubernetes-jenkins/logs/test-infra-cfl-coverage-report/1630446352808808448/"))))
+
+(spit "job-artifacts-test.json"
+      (json/generate-string
+       (successful-jobs "prow-deck.json")))
