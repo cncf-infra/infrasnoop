a1023ab852291df3e6164a3b3ce6511ee2d1837d
zim <webmaster@coolguy.website>
2023-03-06 11:57:38 +1300

    remove temporary designation in init script

--- initdb/507_load_prow_artifact.sql
+++ initdb/507_load_prow_artifact.sql
@@ -9,5 +9,5 @@ select
   regexp_substr(url,'(?<=https://storage.googleapis.com/kubernetes-jenkins/logs/.*/)[0-9]*(?=/)') as build_id,
   url as url
   from job_artifact_import;
-drop temporary table job_artifact_import;
+drop table job_artifact_import;
 commit;
