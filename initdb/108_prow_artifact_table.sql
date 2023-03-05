855c13561c109f4c9dc15bd3aaab2950dcb74e7c
zim <webmaster@coolguy.website>
2023-03-06 11:56:30 +1300

    change url column name

--- initdb/108_prow_artifact_table.sql
+++ initdb/108_prow_artifact_table.sql
@@ -4,7 +4,7 @@ create table prow.artifact(
   id uuid NOT NULL DEFAULT gen_random_uuid() PRIMARY KEY,
   job text,
   build_id text,
-  artifact_url text,
+  url text,
   size text,
   modified text
 );
@@ -13,7 +13,7 @@ comment on table  prow.artifact is 'every artifact link for the most recent succ
 comment on column prow.artifact.id is 'auto generated row id';
 comment on column prow.artifact.job is 'job this artifact applies to';
 comment on column prow.artifact.build_id is 'id of specific running of this job';
-comment on column prow.artifact.artifact_url is 'url of artifact';
+comment on column prow.artifact.url is 'url of artifact';
 comment on column prow.artifact.size is 'size in bytes of artifact';
 comment on column prow.artifact.modified is 'last modified date of artifact';
 
