077a96438028a17cca09df2a17686b626fb731f3
zim <webmaster@coolguy.website>
2023-05-24 14:51:47 +1200

    scrape prow spyglass for direct link to prowspec

--- go-client/go.mod
+++ go-client/go.mod
@@ -3,11 +3,20 @@ module go-client
 go 1.20
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	github.com/jackc/pgx/v5 v5.3.1
+)
+
+require gopkg.in/yaml.v2 v2.4.0 // indirect
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jackc/puddle/v2 v2.2.0 // indirect
 	golang.org/x/crypto v0.6.0 // indirect
+	golang.org/x/net v0.7.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
 	golang.org/x/text v0.7.0 // indirect
+	sigs.k8s.io/yaml v1.3.0
 )
--- go-client/main.go
+++ go-client/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
-	"strings"
 	"fmt"
+	"io"
+	"log"
+	"net/http"
 	"os"
 	"sync"
-	"net/http"
-	// "encoding/json"
-	"io"
 
+	"github.com/PuerkitoBio/goquery"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"sigs.k8s.io/yaml"
 )
 
 var pool *pgxpool.Pool
@@ -19,51 +20,49 @@ func main() {
 	var err error
 	pool, err = pgxpool.New(context.Background(), os.Getenv("DATABASE_URL"))
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Unable to connect to database:", err)
-		os.Exit(1)
+		log.Fatal("Unable to connect to database: ", err)
 	}
 	defer pool.Close()
-	fmt.Println("listening now")
-	for {
-	  listen()
-	}
+	listen()
 }
 
 func listen() {
 	conn, err := pool.Acquire(context.Background())
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error acquiring connection: ", err)
-		os.Exit(1)
+		log.Fatal("Error acquiring connection: ", err)
 	}
-	defer conn.Release();
+	defer conn.Release()
 
 	_, err = conn.Exec(context.Background(), "listen prow") // set up a postgres listen
 	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error listening to prow channel:", err)
-		os.Exit(1)
+		log.Fatal("Error listening to prow channel: ", err)
 	}
+	fmt.Println("listening now")
 
 	for {
-		fmt.Println("hi")
 		notification, err := conn.Conn().WaitForNotification(context.Background())
 		if err != nil {
-			fmt.Fprintln(os.Stderr, "Error waiting for notification: ", err)
+			log.Println("Error wiating for notification: ", err)
 		}
-		fmt.Println("new notification: ", notification.Payload)
+		log.Println("New notification", notification.Payload)
+		log.Println("Fetching prow job specs")
 		if err = getLatestJobYamls(); err != nil {
-			fmt.Fprintln(os.Stderr, "Error getting job yamls: ", err)
+			log.Println("Error getting job yamls: ", err)
 		}
+		log.Println("Job specs gathered. Listening...")
 	}
 }
 
-func getLatestJobYamls () error {
+func getLatestJobYamls() error {
 	var wg sync.WaitGroup
 	conn, err := pool.Acquire(context.Background())
 	defer conn.Release()
 	if err != nil {
 		return err
 	}
-	rows, err := conn.Query(context.Background(), "select job,build_id, url from prow.latest_success;")
+	latestSuccessQuery := "select job, build_id, url from prow.latest_success"
+
+	rows, err := conn.Query(context.Background(), latestSuccessQuery)
 	if err != nil {
 		return err
 	}
@@ -72,35 +71,89 @@ func getLatestJobYamls () error {
 		var job string
 		var build_id string
 
-		if err := rows.Scan(&job,&build_id,&url); err != nil {
+		if err := rows.Scan(&job, &build_id, &url); err != nil {
 			return err
 		}
-		go func(job string,build_id string, url string) {
-			wg.Add(1)
-			jobArtifactUrl := strings.Replace(url, "prow.k8s.io/view/gs/","storage.googleapis.com/",1)
-			jobSpecUrl := strings.TrimRight(jobArtifactUrl,"/") + "/prowjob.json"
-			resp, err := http.Get(jobSpecUrl)
+		wg.Add(1)
+		go func(job string, build_id string, url string) {
+			prowspec, err := getProwSpec(url)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error getting url: ", err)
-			}
-			data, err := io.ReadAll(resp.Body)
-			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error reading body: ", err)
+				log.Println("Error getting prowspec: ", err)
 			}
 			conn, err := pool.Acquire(context.Background())
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "Error acquiring connection: ", err)
-				os.Exit(1)
+				log.Fatal("Error acquiring connection: ", err)
 			}
-			defer conn.Release();
-			_, err = conn.Exec(context.Background(), "call upsertJobSpec($1,$2,$3)", job,build_id,string(data))
+			defer conn.Release()
+
+			upsertCall := "call upsertJobSpec($1,$2,$3)" //job,build_id,prowspec
+			_, err = conn.Exec(context.Background(), upsertCall, job, build_id, prowspec)
 			if err != nil {
-				fmt.Fprintln(os.Stderr, "error adding spec: ", url,err)
+				log.Println("error adding spec: ", url, err)
 			}
-			fmt.Println("added spec for ",job,", build", build_id)
+			log.Println("added spec for ", job, ", build", build_id)
 			wg.Done()
 		}(job, build_id, url)
 	}
 	wg.Wait()
-	return nil
+	return err
+}
+
+func getProwSpec(url string) (spec string, err error) {
+	res, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		err = fmt.Errorf("Did not get 200 status code: %v", err)
+		return "", err
+	}
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		err = fmt.Errorf("goquery could not parse url body(%v):%v", url, err)
+		return "", err
+	}
+
+	var prowYamlLink string
+	doc.Find("#links-card a").Each(func(i int, s *goquery.Selection) {
+		content := s.Text()
+		if content == "Prow Job YAML" {
+			path, ok := s.Attr("href")
+			if !ok {
+				fmt.Fprintln(os.Stderr, "Found prow job link, but it has no href? ", url)
+			}
+			prowYamlLink = "https://prow.k8s.io" + path
+		}
+	})
+	if prowYamlLink == "" {
+		err := fmt.Errorf("Could not find a link to yaml for %v", url)
+		return "", err
+	}
+
+	jsonString, err := prowAsJson(prowYamlLink)
+	if err != nil {
+		err = fmt.Errorf("Could not get prow spec as json string(%v): %v", url, err)
+		return "", err
+	}
+	return jsonString, nil
+}
+
+func prowAsJson(url string) (jsonString string, err error) {
+	res, err := http.Get(url)
+	if err != nil {
+		err := fmt.Errorf("Error getting link from given url %v: %v", url, err)
+		return "", err
+	}
+	yamldata, err := io.ReadAll(res.Body)
+	if err != nil {
+		err = fmt.Errorf("Error reading body of yaml: ", err)
+		return "", err
+	}
+	jsonData, err := yaml.YAMLToJSON(yamldata)
+	if err != nil {
+		err = fmt.Errorf("Could not parse yaml to json for %v: %v", url, err)
+		return "", err
+	}
+	return string(jsonData), err
 }
